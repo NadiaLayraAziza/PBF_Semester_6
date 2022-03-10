f0223ef2a8e26532f5def9a975e95234b54abf15
Nadia Layra Aziza <nadialayra15@gmail.com>
2022-03-10 14:06:05 +0700

    Praktikum 3 - React Component Update

--- Pertemuan 3/web-reactku/src/index.js
+++ Pertemuan 3/web-reactku/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import HelloComponent from "./component/HelloComponent";
+// import StateFullComponent from "./container/StateFullComponent";
 import LoginComponent from "./component/LoginComponent";
 
 ReactDOM.render(<LoginComponent />, document.getElementById("root"));
