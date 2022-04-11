a29a78cdc05d848386242e11571d0c222f5304a4
Nadia Layra Aziza <nadialayra15@gmail.com>
2022-04-11 22:39:27 +0700

    Modul 7 - HOC

---
+++ Pertemuan 7/hoc-react/src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { createStore, combineReducers, compose } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import { Provider } from 'react-redux';
+import GenericContainer from './GenericContainer/GenericContainerDemo.jsx';
+import LoaderDemo from './LoadDemo/LoaderDemo';
+
+const rootReducer = combineReducers({
+  form: formReducer,
+});
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+  <LoaderDemo />
+  </Provider>,
+  document.getElementById('root')
+);
+
+/* ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+); */
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
