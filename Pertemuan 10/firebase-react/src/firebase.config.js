dd99514d4390846934c5fc0bfec2207d4e3246fe
Nadia Layra Aziza <nadialayra15@gmail.com>
2022-05-09 19:01:49 +0700

    Modul 10 - Praktikum Firebase React

---
+++ Pertemuan 10/firebase-react/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAbCaAdClR9pflBBd4FoAp7l1FyDUeq6fA",
+    authDomain: "fir-react-js-nadia.firebaseapp.com",
+    projectId: "fir-react-js-nadia",
+    storageBucket: "fir-react-js-nadia.appspot.com",
+    messagingSenderId: "632732654590",
+    appId: "1:632732654590:web:eb51b545db6ffe9c6afeaf",
+    measurementId: "G-G9W2ESV05R"
+};
