811593330f2e9fc19307876aa3639f2955555887
Nadia Layra Aziza <nadialayra15@gmail.com>
2022-06-02 13:00:46 +0700

    Pertemuan 12 - Firebase CRUD

---
+++ Pertemuan 12/firebase-crud/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    // Your config values
+    apiKey: "AIzaSyCznrQai42JZ1-kCLnQ-BmBltDMXvTfKS4",
+    authDomain: "fir-login-nadia.firebaseapp.com",
+    projectId: "fir-login-nadia",
+    storageBucket: "fir-login-nadia.appspot.com",
+    messagingSenderId: "498100703771",
+    appId: "1:498100703771:web:9d54505a167aba93c55ebe",
+    measurementId: "G-3716JKHZV5"
+};
+
+export default firebaseConfig;
